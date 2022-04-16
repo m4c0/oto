91f4d11f64acd2bb74e865e2851c3759befe4654
Eduardo Costa <m4c0@users.noreply.github.com>
2022-04-16 13:34:43 +0100

    dumping actors

--- lib/dumper.ml
+++ lib/dumper.ml
@@ -4,15 +4,30 @@ let rec print_yaml (fn : Types.scene) =
     print_string ": ";
     print_endline v
   in
+  let print_val p v =
+    print_string p;
+    print_endline v
+  in
+  let print_cast lbl = function
+    | [] -> ()
+    | l ->
+        print_string "  ";
+        print_string lbl;
+        print_endline ":";
+        List.iter (print_val "    - ") l
+  in
   let meta, next = fn () in
+  let cast = meta.actors () in
   print_kv "name" meta.name;
   Option.iter (print_kv "background") meta.background;
   Option.iter (print_kv "music") meta.music;
+  print_endline "actors:";
+  print_cast "left" cast.left;
+  print_cast "middle" cast.middle;
+  print_cast "right" cast.right;
   print_endline "---";
   match next with
   | Continuation s -> print_yaml s
   | WaitThenJump s -> print_yaml s
   | Choice l -> List.iter (fun (_, s) -> print_yaml s) l
   | EndGame -> ()
-
-
--- test/poc.ml
+++ test/poc.ml
@@ -1,18 +1,21 @@
 open Oto
 
+let lefty = "lefty"
+let midly = "midly"
+let righty = "righty"
 let title_meta = scene_meta ~background:"title" ~music:"romance-1" "Title"
 let lefty_meta = scene_meta "Lefty Intro"
 let midly_meta = scene_meta "Midly Intro"
 let righty_meta = scene_meta "Righty Intro"
-let lefty_says = speak "lefty"
-let midly_says = speak "midly"
-let righty_says = speak "righty"
-let midly_poses = pose "midly"
+let lefty_says = speak lefty
+let midly_says = speak midly
+let righty_says = speak righty
+let midly_poses = pose midly
 let restaurant_meta = scene_meta ~background:"restaurant" ~music:"romance-1"
 
 let intro_meta =
   restaurant_meta "Intro"
-    ~actors:(simple_cast ~left:"lefty" ~middle:"middly" ~right:"righty")
+    ~actors:(simple_cast ~left:lefty ~middle:midly ~right:righty)
     ~script:
       [
         lefty_says "Hello! How are you doing?";
@@ -23,12 +26,13 @@ let intro_meta =
         midly_says "I'm as hot as an idol";
       ]
 
-let lefty _ = then_endgame lefty_meta
-let midly _ = then_endgame midly_meta
-let righty _ = then_endgame righty_meta
-
 let intro _ =
-  then_choose intro_meta [ ("Lefty", lefty); ("Midly", midly); ("Righty", righty) ]
+  then_choose intro_meta
+    [
+      ("Lefty", fun _ -> then_endgame lefty_meta);
+      ("Midly", fun _ -> then_endgame midly_meta);
+      ("Righty", fun _ -> then_endgame righty_meta);
+    ]
 
 let game : Types.scene = fun _ -> then_continue title_meta intro
 let () = Dumper.print_yaml game
