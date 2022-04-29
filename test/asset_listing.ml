df3059087c6ab538f27f8bd421bd55f9013d86a8
Eduardo Costa <m4c0@users.noreply.github.com>
2022-04-29 21:33:53 +0100

    navigation for the asset listing

--- test/asset_listing.ml
+++ test/asset_listing.ml
@@ -1,8 +1,12 @@
-let print_assets (s : Oto.Types.scene) =
-  let ({ background; music; _ } : Oto.Types.scene_meta), _ = s () in
+let rec print_assets (s : Oto.Types.scene) =
+  let ({ background; music; _ } : Oto.Types.scene_meta), next = s () in
   let print_asset tp file = Filename.concat tp file |> print_endline in
   let print_opt_asset tp file = Option.iter (print_asset tp) file in
   print_opt_asset "back" background;
-  print_opt_asset "music" music
+  print_opt_asset "music" music;
+  match next with
+  | Continuation s | WaitThenJump s -> print_assets s
+  | Choice l -> List.map (fun (_, s) -> s) l |> List.iter print_assets
+  | EndGame -> ()
 
 let () = print_assets Poc.game
