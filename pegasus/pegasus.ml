ab54d318129f8dc73cda046a3b9d41d36450d7b5
Eduardo Costa <m4c0@users.noreply.github.com>
2022-05-19 19:22:17 +0100

    pegasus accepting a scene as a parameter

--- pegasus/pegasus.ml
+++ pegasus/pegasus.ml
@@ -1,35 +1,40 @@
-let width = 640
-let height = 480
-let flags = []
-let clear_color = (0, 0, 0)
+module type Specs = sig
+  val width : int
+  val height : int
+end
 
-let run () =
-  Sdl.init [ `VIDEO ];
-  let _, renderer =
-    Sdl.Render.create_window_and_renderer ~width ~height ~flags
-  in
+module M (S : Specs) (D : Oto.Domain) = struct
+  let clear_color = (0, 0, 0)
 
-  let dispatch = function
-    | Sdl.Event.Quit _ ->
-        Sdl.quit ();
-        exit 0
-    | _ -> ()
-  in
-  let rec event_loop () =
-    match Sdl.Event.poll_event () with
-    | None -> ()
-    | Some e ->
-        dispatch e;
-        event_loop ()
-  in
-  let render () =
-    Sdl.Render.set_draw_color renderer ~rgb:clear_color ~a:255;
-    Sdl.Render.clear renderer;
-    Sdl.Render.render_present renderer
-  in
-  let rec main_loop () =
-    event_loop ();
-    render ();
+  let run (_ : Oto.M(D).scene) =
+    Sdl.init [ `VIDEO ];
+    let _, renderer =
+      Sdl.Render.create_window_and_renderer ~width:S.width ~height:S.height
+        ~flags:[]
+    in
+
+    let dispatch = function
+      | Sdl.Event.Quit _ ->
+          Sdl.quit ();
+          exit 0
+      | _ -> ()
+    in
+    let rec event_loop () =
+      match Sdl.Event.poll_event () with
+      | None -> ()
+      | Some e ->
+          dispatch e;
+          event_loop ()
+    in
+    let render () =
+      Sdl.Render.set_draw_color renderer ~rgb:clear_color ~a:255;
+      Sdl.Render.clear renderer;
+      Sdl.Render.render_present renderer
+    in
+    let rec main_loop () =
+      event_loop ();
+      render ();
+      main_loop ()
+    in
     main_loop ()
-  in
-  main_loop ()
+end
--- test/pegasus.ml
+++ test/pegasus.ml
@@ -1,1 +1,8 @@
-let () = Pegasus.run ()
+module S = struct
+  let width = 800
+  let height = 600
+end
+
+module P = Pegasus.M (S) (Poc.Printer)
+
+let () = P.run Poc.game
