6ac726f367e5c7fddd0023d6de674a960c0fd328
Eduardo Costa <m4c0@users.noreply.github.com>
2022-05-20 11:43:25 +0100

    pegasus accepting any_key to advance "present"

--- pegasus/cindel.ml
+++ pegasus/cindel.ml
@@ -1,3 +1,5 @@
+type state = { any_key_down : bool }
+
 let init width height =
   Sdl.init [ `VIDEO ];
   let _, renderer =
@@ -5,13 +7,18 @@ let init width height =
   in
   renderer
 
-let rec event_loop renderer =
-  match Sdl.Event.poll_event () with
-  | None ->
-      Sdl.Render.set_draw_color renderer ~rgb:(255, 0, 255) ~a:255;
-      Sdl.Render.clear renderer;
-      Sdl.Render.render_present renderer
-  | Some (Sdl.Event.Quit _) ->
-      Sdl.quit ();
-      exit 0
-  | Some _ -> event_loop renderer
+let event_loop renderer =
+  let rec loop state =
+    match Sdl.Event.poll_event () with
+    | None ->
+        Sdl.Render.set_draw_color renderer ~rgb:(255, 0, 255) ~a:255;
+        Sdl.Render.clear renderer;
+        Sdl.Render.render_present renderer;
+        state
+    | Some (Sdl.Event.Quit _) ->
+        Sdl.quit ();
+        exit 0
+    | Some (Sdl.Event.KeyUp _) -> loop { any_key_down = true }
+    | Some _ -> loop state
+  in
+  loop { any_key_down = false }
--- pegasus/pegasus.ml
+++ pegasus/pegasus.ml
@@ -9,9 +9,26 @@ module M (S : Specs) (D : Oto.Domain) = struct
   let run (s : Oto.scene) =
     let renderer = Cindel.init S.width S.height in
 
+    let run_action : Oto.action -> unit = function
+      | Background _ -> print_endline "bg"
+      | Choose _ -> print_endline "choo choo"
+      | Music _ -> print_endline "mus"
+      | Pause -> print_endline "pause"
+      | Present -> print_endline "present"
+      | Speak _ -> print_endline "woof"
+    in
+    let run_actions (vm : Oto.t) (s : Cindel.state) =
+      match vm () with
+      | Cons (Present, next) -> if s.any_key_down then next else vm
+      | Cons (a, next) ->
+          run_action a;
+          next
+      | Nil -> vm
+    in
+
     let rec main_loop vm =
-      Cindel.event_loop renderer;
-      main_loop vm
+      let state = Cindel.event_loop renderer in
+      run_actions vm state |> main_loop
     in
     Oto.from_scene s |> main_loop
 end
