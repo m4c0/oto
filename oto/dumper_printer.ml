ebda4fbd8997c18587fd874b6f23bae399bea012
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-06 19:09:09 +0100

    cleaning up more ocaml code

--- oto/dumper_printer.ml
+++
@@ -1,5 +0,0 @@
-module type M = sig
-  type actor
-
-  val actor_to_string : actor -> string
-end
--- oto/oto.ml
+++
@@ -1,5 +0,0 @@
-module type Domain = Types.Domain
-
-module Builder = Builder.M
-module Types = Types.M
-module Vm = Vm.M
