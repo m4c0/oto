e498222a6622d274c8385336175c8a5cdd10d75e
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-07 23:21:11 +0100

    sketch for the frame_state

---
+++ src/frame_state.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace oto {
+  struct frame_state {
+    bool any_key_down;
+  };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include "casein.sdl.hpp"
+#include "frame_state.hpp"
 #include "sdl.hpp"
 
 #include <memory>
 
-void casein_sdl_event(const casein::event & e) {
+void casein_sdl_event(const casein::event & evt) {
+  static oto::frame_state frame_state;
   static std::unique_ptr<oto::sdl> sdl;
 
-  switch (e.type()) {
+  switch (evt.type()) {
   case casein::CREATE_WINDOW:
-    sdl = std::make_unique<oto::sdl>(e.as<casein::events::create_window>().native_window_handle());
+    sdl = std::make_unique<oto::sdl>(evt.as<casein::events::create_window>().native_window_handle());
     break;
   case casein::REPAINT:
     if (sdl) sdl->repaint();
+    frame_state = {};
     break;
   default:
     break;
