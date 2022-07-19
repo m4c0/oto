c7acbdfc212923e74220c3076132fa7f85290e4e
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-19 10:07:31 +0100

    tidying up

--- test/poc.cpp
+++ test/poc.cpp
@@ -1,57 +1,61 @@
 #include "oto/engine.hpp"
 #include "poc.hpp"
 
+static constexpr const auto HALF_VALUE = 128;
+
 static void music_callback(std::span<float> /**/) {
 }
 
-static constexpr const unsigned act_color(poc::domain::actor act) {
+static constexpr unsigned act_color(poc::domain::actor act) {
   switch (act) {
   case poc::domain::lefty:
-    return 0xaa00;
+    return 0xaa00; // NOLINT
   case poc::domain::midly:
-    return 0xaa0000;
+    return 0xaa0000; // NOLINT
   case poc::domain::midly_hot:
-    return 0xff0000;
+    return 0xff0000; // NOLINT
   case poc::domain::righty:
-    return 0xaa000000;
+    return 0xaa000000; // NOLINT
   }
 }
-static constexpr const unsigned bg_color(poc::domain::background bck) {
+static constexpr unsigned bg_color(poc::domain::background bck) {
   switch (bck) {
   case poc::domain::title:
-    return 0x7700;
+    return 0x7700; // NOLINT
   case poc::domain::restaurant:
-    return 0x770000;
+    return 0x770000; // NOLINT
   case poc::domain::end:
-    return 0x77000000;
+    return 0x77000000; // NOLINT
   }
 }
 struct asset {
   static oto::texture load_actor(poc::domain::actor act) {
-    return oto::r::create_color_texture(128, 128, act_color(act));
+    return oto::r::create_color_texture(HALF_VALUE, HALF_VALUE, act_color(act));
   }
   static oto::texture load_background(poc::domain::background bck) {
-    return oto::r::create_color_texture(128, 128, bg_color(bck));
+    return oto::r::create_color_texture(HALF_VALUE, HALF_VALUE, bg_color(bck));
   }
-  static auto load_music(poc::domain::music bck) {
+  static auto load_music(poc::domain::music /**/) {
     return music_callback;
   }
   static oto::texture load_text_background(poc::domain::background bck) {
-    switch (bck) {
-    case poc::domain::restaurant:
-      return oto::r::create_color_texture(128, 128, 0);
-    default:
-      return nullptr;
+    if (bck == poc::domain::restaurant) {
+      return oto::r::create_color_texture(HALF_VALUE, HALF_VALUE, 0);
     }
+    return nullptr;
   }
   static auto rect_of_side(poc::domain::side side) {
+    static constexpr const auto ACTOR_H = 400;
+    static constexpr const auto ACTOR_W = 160;
+    static constexpr const auto ACTOR_Y = 100;
+
     switch (side) {
     case poc::domain::left:
-      return oto::rect { 160, 100, 160, 400 };
+      return oto::rect { ACTOR_W * 1, ACTOR_Y, ACTOR_W, ACTOR_H };
     case poc::domain::middle:
-      return oto::rect { 320, 100, 160, 400 };
+      return oto::rect { ACTOR_W * 2, ACTOR_Y, ACTOR_W, ACTOR_H };
     case poc::domain::right:
-      return oto::rect { 480, 100, 160, 400 };
+      return oto::rect { ACTOR_W * 3, ACTOR_Y, ACTOR_W, ACTOR_H };
     }
   }
 };
