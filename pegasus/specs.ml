0be94747a07f2814488b7dc8aff22f3e00e19218
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-20 17:51:25 +0100

    more ocaml cleanup

--- pegasus/specs.ml
+++
@@ -1,8 +0,0 @@
-module type M = sig
-  module D : Oto.Domain
-
-  val width : int
-  val height : int
-  val load_background : D.background -> Cindel.surface
-  val load_music : D.music -> Cindel.audio_callback
-end
--- pegasus/ticks.ml
+++
@@ -1,5 +0,0 @@
-type t = int
-
-let epoch = 0
-let now _ = Sdl.Timer.get_ticks ()
-let ms_from_now (n : t) = now () - n
