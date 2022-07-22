36f9cd3519764102e50f81bdcad2623519f7b3e9
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-22 10:15:14 +0100

    wasm: using stubs for renderer

---
+++ wasm/extern.cpp
@@ -0,0 +1,30 @@
+#include "oto/renderer.hpp"
+
+void oto::r::init(void * /**/) {
+}
+
+void oto::r::deleter::operator()(oto::r::texture_ptr *) const {
+}
+
+oto::texture oto::r::create_rgba_texture(int w, int h, std::span<const unsigned> data) {
+  return {};
+}
+
+oto::texture oto::r::create_color_texture(int w, int h, unsigned color) {
+  return {};
+}
+
+void oto::r::draw(const oto::texture & txt) {
+}
+
+void oto::r::draw(const oto::texture & txt, const oto::rect & rect) {
+}
+
+void oto::r::draw(const oto::texture & txt, const oto::rect & clip, const oto::rect & target) {
+}
+
+void oto::r::draw_string(const oto::texture & font, const oto::size & chr_size, std::string_view str, int x, int y) {
+}
+
+void oto::r::set_audio_callback(oto::r::audio_callback_t cbk) {
+}
