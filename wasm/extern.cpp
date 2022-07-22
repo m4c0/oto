560b4c133e4f36217ef9f25f624330f80148f605
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-22 10:35:08 +0100

    wasm: creating textures

--- wasm/extern.cpp
+++ wasm/extern.cpp
@@ -3,15 +3,24 @@
 void oto::r::init(void * /**/) {
 }
 
-void oto::r::deleter::operator()(oto::r::texture_ptr *) const {
+void oto::r::deleter::operator()(oto::r::texture_ptr * ptr) const {
+  free(reinterpret_cast<unsigned *>(ptr));
 }
 
 oto::texture oto::r::create_rgba_texture(int w, int h, std::span<const unsigned> data) {
-  return {};
+  auto size = w * h * sizeof(unsigned);
+  void * res = malloc(size);
+  memcpy(res, data.begin(), size);
+  return oto::texture(reinterpret_cast<oto::r::texture_ptr *>(res));
 }
 
 oto::texture oto::r::create_color_texture(int w, int h, unsigned color) {
-  return {};
+  auto size = w * h;
+  void * res = malloc(size * sizeof(unsigned));
+  unsigned * ptr = static_cast<unsigned *>(res);
+  while (size--)
+    *ptr++ = color;
+  return oto::texture(reinterpret_cast<oto::r::texture_ptr *>(res));
 }
 
 void oto::r::draw(const oto::texture & txt) {
