155b8ef7c1e5de3ea5c5603e8249c99629abf6eb
Eduardo Costa <m4c0@users.noreply.github.com>
2022-07-22 09:44:13 +0100

    expanding wasm example

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -2,5 +2,21 @@
 
 extern "C" void loggy(const char *);
 void casein_event(const casein::event & e) {
-  loggy("ok");
+  switch (e.type()) {
+  case casein::CREATE_WINDOW:
+    loggy("created");
+    break;
+  case casein::REPAINT:
+    loggy("repaint");
+    break;
+  case casein::KEY_DOWN:
+    loggy("up");
+    break;
+  case casein::KEY_UP:
+    loggy("down");
+    break;
+  case casein::QUIT:
+    loggy("quit");
+    break;
+  }
 }
